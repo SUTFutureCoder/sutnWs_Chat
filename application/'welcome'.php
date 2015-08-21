35cdcd3fd5da8c704bda3ba6965b53fbcd73122b
linxingchen <linxingchen@baidu.com>
2015-08-21 11:06:40 +0800

    init commit

---
+++ application/'welcome'.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/**
+ * 
+ * 
+ * 
+ *
+ * @copyright  版权所有(C) 2014-2015 沈阳工业大学ACM实验室 沈阳工业大学网络管理中心 *Chen
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt   GPL3.0 License
+ * @version    
+ * @link       https://github.com/SUTFutureCoder/
+*/
