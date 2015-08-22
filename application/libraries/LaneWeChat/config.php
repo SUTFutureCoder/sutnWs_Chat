6ebf5ed9b87f268f4312c912f7c773d32d409dd3
linxingchen <linxingchen@baidu.com>
2015-08-22 20:10:09 +0800

    init #2 open libraries

--- application/libraries/LaneWeChat/config.php
+++ application/libraries/LaneWeChat/config.php
@@ -29,31 +29,31 @@ define("WECHAT_APPSECRET", '0ddbd5bb920d17e16a4c5601317ba2c9');
 
 ////-----引入系统所需类库-------------------
 ////引入错误消息类
-//include_once 'core/msg.lib.php';
+include_once 'core/msg.lib.php';
 ////引入错误码类
-//include_once 'core/msgconstant.lib.php';
+include_once 'core/msgconstant.lib.php';
 ////引入CURL类
-//include_once 'core/curl.lib.php';
+include_once 'core/curl.lib.php';
 //
 ////-----------引入微信所需的基本类库----------------
 ////引入微信处理中心类
-//include_once 'core/wechat.lib.php';
+include_once 'core/wechat.lib.php';
 ////引入微信请求处理类
-//include_once 'core/wechatrequest.lib.php';
+include_once 'core/wechatrequest.lib.php';
 ////引入微信被动响应处理类
-//include_once 'core/responsepassive.lib.php';
+include_once 'core/responsepassive.lib.php';
 ////引入微信access_token类
-//include 'core/accesstoken.lib.php';
+include 'core/accesstoken.lib.php';
 //
 ////-----如果是认证服务号，需要引入以下类--------------
 ////引入微信权限管理类
-//include_once 'core/wechatoauth.lib.php';
+include_once 'core/wechatoauth.lib.php';
 ////引入微信用户/用户组管理类
-//include_once 'core/usermanage.lib.php';
+include_once 'core/usermanage.lib.php';
 ////引入微信主动相应处理类
-//include_once 'core/responseinitiative.lib.php';
+include_once 'core/responseinitiative.lib.php';
 ////引入多媒体管理类
-//include_once 'core/media.lib.php';
+include_once 'core/media.lib.php';
 ////引入自定义菜单类
-//include_once 'core/menu.lib.php';
+include_once 'core/menu.lib.php';
 ?>
