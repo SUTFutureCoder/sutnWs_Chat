af297440308057bbe7c46d126ff7c28d46db5e1e
linxingchen <linxingchen@baidu.com>
2015-09-04 00:26:16 +0800

    rename redis

--- application/libraries/MyRedis.php
+++ application/libraries/MyRedis.php
@@ -12,7 +12,7 @@ if (!defined('BASEPATH'))
  * @version    
  * @link       https://github.com/SUTFutureCoder/
 */
-class MyRedis{
+class Myredis{
     private static $redis;
     
     public function getInstance(){
