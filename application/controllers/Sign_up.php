f31fb089de8bef09ed5beed71ddd24b340625c06
NBSUT <2484611837@qq.com>
2015-09-10 05:57:55 +0800

    debug

--- application/controllers/Sign_up.php
+++ application/controllers/Sign_up.php
@@ -71,7 +71,7 @@ class Sign_up extends CI_Controller{
             
            $user_id = $this->user_model->user_sign_up($user_info, $user_sec_info);
                  if($user_id ){
-                $url = base_url().'pqcode/'.$user_id.'png';
+                $url = base_url().'pqcode/'.$user_id.'.png';
                 
                 $text = site_url().'/interviemer/index?user_id='.$user_id.'&userName='.$user_info['user_name'].'&userTelephone='.$user_info['user_telephone'].'&userQQ='.$user_info['user_qq'].'&userNumber='.$user_info['user_number'].'&userMajor='.$user_info['user_major'].'&userSex='.$user_info['user_sex'].'&user_talent='.$user_info['user_talent'];
 
